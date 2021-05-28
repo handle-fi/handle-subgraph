28f7c0f4edc67eba67b6e5769acfc18d396bad5c
Lorenzo Fontoura <lorenzo@nioel.com>
2021-05-28 16:22:54 +1000

    update handle mapping

--- src/mappings/handle.ts
+++ src/mappings/handle.ts
@@ -1,8 +1,11 @@
 ﻿import {
   UpdateCollateral as UpdateCollateralEvent,
   UpdateDebt as UpdateDebtEvent,
+  ConfigureCollateralToken as ConfigureCollateralTokenEvent,
+  ConfigureFxToken as ConfigureFxTokenEvent,
 } from "../types/Handle/Handle";
 import * as vault from "./handle/vault";
+import * as tokens from "./handle/tokens";
 
 export function handleDebtUpdate (event: UpdateDebtEvent): void {
   vault.handleDebtUpdate(event);
@@ -10,4 +13,12 @@ export function handleDebtUpdate (event: UpdateDebtEvent): void {
 
 export function handleCollateralUpdate (event: UpdateCollateralEvent): void {
   vault.handleCollateralUpdate(event);
+}
+
+export function handleFxTokenConfiguration (event: ConfigureFxTokenEvent): void {
+  tokens.handleFxTokenConfiguration(event);
+}
+
+export function handleCollateralTokenConfiguration (event: ConfigureCollateralTokenEvent): void {
+  tokens.handleCollateralTokenConfiguration(event);
 }
